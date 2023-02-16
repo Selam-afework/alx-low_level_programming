6e1cd5c143ed03887bb1b5adf66306a13c2aa23a
Selam-afework <selam.a.gorfu@gmail.com>
2023-02-16 10:13:29 -0800

    q.4 9th times

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	char Mystr [] = ""Programming is like building a multinigual puzzle";
+	char Mystr[] = "\"Programming is like building a multinigual puzzle";
 	puts(Mystr);
 	return 0;
 }
