966e753837e037e94dddf48cbe87a8333bb44882
Selam-afework <selam.a.gorfu@gmail.com>
2023-02-16 10:02:39 -0800

    q.4 7th times

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main(void)
- {
-	 char Mystr [] = "Programming is like building a multinigual puzzle";
- 	 puts(Mystr);
-	 return 0;
+int main(void) // main = main function
+{
+	char Mystr [] = ""Programming is like building a multinigual puzzle";
+	puts(Mystr);
+	return 0;
 }
