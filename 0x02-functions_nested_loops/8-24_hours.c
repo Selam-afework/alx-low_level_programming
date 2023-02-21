b403140871b8f1823e1b588bcf23d9da0e4347f1
Selam-afework <selam.a.gorfu@gmail.com>
2023-02-21 14:20:53 -0800

    q.8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * jack_bauer - to print every minute of the day
+ *
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+{
+	int x = 00;
+	while (x <= 23)
+	{
+		return (x);
+	}
+}
