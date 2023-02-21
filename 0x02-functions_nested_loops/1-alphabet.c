560ea1d40f737c97366bf5405280644875518a38
Selam-afework <selam.a.gorfu@gmail.com>
2023-02-21 10:17:36 -0800

    q.1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 'Success'
+ */
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+
+		alphabet++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
