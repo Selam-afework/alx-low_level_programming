c0c4fcc93c67a70d24ce42f8ae4ffabd0f35b201
Selam-afework <selam.a.gorfu@gmail.com>
2023-02-22 08:24:59 -0800

    q.12 bonus

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,10 +9,10 @@
 
 void print_times_table(int n)
 {
-	for (n = 0; n < 15; n++)
-	{
-		int p = n;
+	int p, z;
 
+	if ((n >= 0) && (n <= 15))
+	{
 		for (p = 0; p <= n; p++)
 		{
 			int z = (n * p);
@@ -24,14 +24,23 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar(z + '0');
 			}
-			else if (z >= 10)
+			else if ((z >= 10) && (z < 100))
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar((z / 10) + '0');
 				_putchar((z % 10) + '0');
 			}
+			else if (z >= 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((z / 100) + '0');
+				_putchar(((z / 10) % 10) + '0');
+				_putchar((z % 10) + '0');
+			}
 		}
+		n++;
 		_putchar('\n');
 	}
 }
