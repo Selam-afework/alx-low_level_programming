7be145916c83a7a48ae7d95536dbecf1c4ee1a46
Selam-afework <selam.a.gorfu@gmail.com>
2023-02-22 09:20:54 -0800

    14th times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,48 +6,45 @@
  *
  * Return: Always 0 (Success)
  */
-
 void print_times_table(int n)
 {
 	int p, y, z;
 
 	if ((n >= 0) && (n <= 15))
+	for (y = 0; y <= n; y++)
 	{
-		for (y = 0; y <= n; y++)
+		for (p = 0; p <= n; p++)
 		{
-			for (p = 0; p <= n; p++)
+			z = (y * p);
+			if (p == 0)
 			{
-				z = (y * p);
-				if (p == 0)
-				{
-					_putchar(z + '0');
-				}
-				if ((z < 10) && (p != 0))
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(z + '0');
-				}
-				else if ((z >= 10) && (z < 100))
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((z / 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else if (z >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
+				_putchar(z + '0');
 			}
-			_putchar('\n');
+			if ((z < 10) && (p != 0))
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else if ((z >= 10) && (z < 100))
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+			else if (z >= 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((z / 100) + '0');
+				_putchar(((z / 10) % 10) + '0');
+				_putchar((z % 10) + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
