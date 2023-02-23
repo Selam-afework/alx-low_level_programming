b24e3fd3e2e107329dc1ddbde08c6cb91cbf6cef
Selam-afework <selam.a.gorfu@gmail.com>
2023-02-23 13:06:11 -0800

    putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+}
