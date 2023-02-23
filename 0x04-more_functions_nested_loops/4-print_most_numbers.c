157d8f7d8a96bb1606cb204b803941deaa84d1f5
Selam-afework <selam.a.gorfu@gmail.com>
2023-02-23 13:49:06 -0800

    correct function name

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * print_numbers - prints numbers from 0 to 9
+ * print_most_numbers - prints numbers from 0 to 9 except 2 & 4
  *
  * Return: Void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
