48e2aa976cfba1f25420fcd7885734b6d489a680
Selam-afework <selam.a.gorfu@gmail.com>
2023-02-23 16:40:19 -0800

    printing fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0 && (n % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((n % 5) == 0 && (n % 3) != 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((n % 3) == 0 && (n % 5) != 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("\n");
+}
