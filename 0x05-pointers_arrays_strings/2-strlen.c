6a804ab3048b7ef24b66acb433fb5bbb621bf6ff
Selam-afework <selam.a.gorfu@gmail.com>
2023-02-27 11:40:12 -0800

    using count variable

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s:  a string being evaluated
+ *
+ * Return: Always 0 'Success'
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; ++count)
+	{
+		return count;
+	}
+}
