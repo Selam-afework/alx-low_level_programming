d4328666e086064cc61863c4b1c37c512ed9293f
Selam-afework <selam.a.gorfu@gmail.com>
2023-02-27 13:17:28 -0800

    introducing new variables

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,30 @@
 #include "main.h"
 
 /**
- * rev_string: a function that reverses a string
+ * rev_string - a function that reverses a string
  *
- * @S: variable to be reversed
+ * @s: variable to be reversed
  * Return: Always 0 (Success)
  */
 
 void rev_string(char *s)
 {
-	int n = 0;
+	int m, n, p;
 
-	int p;
+	char x1, x2;
 
 	for (p = 0; s[p] != '\0'; p++)
 	{
 	}
-
-	for (n = p - 1; n >= 0; n--)
+	m = p - 1;
+	n = 0;
+	while (m > n)
 	{
-		int z; 
-		z = (s[n]);
+		x1 = s[n];
+		x2 = s[m];
+		s[n] = x2;
+		s[m] = x1;
+		m--;
+		n++;
 	}
 }
