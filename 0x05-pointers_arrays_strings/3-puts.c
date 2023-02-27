5fe114a39cc6f543a78f8bebf8233735df0550d2
Selam-afework <selam.a.gorfu@gmail.com>
2023-02-27 12:08:56 -0800

    putchar function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ *
+ * @str: a string to be printed
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+void _puts(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		_putchar(*ptr);
+		*ptr++;
+	}
+}	
