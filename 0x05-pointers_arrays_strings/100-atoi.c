cb19e83e9fda8381d83e6d854c06b24346f3723f
Selam-afework <selam.a.gorfu@gmail.com>
2023-02-28 01:28:16 -0800

    converting string to numbers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - to convert a string to an integer
+ *
+ * @s: a string to be converted
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _atoi(char *s)
+{
+	int i;
+
+	int num = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		num = num * 10 + (s[i] - 48);
+	}
+	printf("%d\n", num);
+	return (0);
+}
