4f3e2efb5130b5a191dd3ceb78ecd2f0be458bfb
Selam-afework <selam.a.gorfu@gmail.com>
2023-03-02 12:16:16 -0800

    q.2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strncpy - to copy a string
+ *
+ * @dest: string no one
+ *
+ * @src: string no two
+ *
+ * @n: an integer
+ *
+ * Result: dest
+ *
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
