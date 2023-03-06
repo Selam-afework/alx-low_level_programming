83f4d676b9569e379024cbbc5a7ba89901eb18b8
Selam-afework <selam.a.gorfu@gmail.com>
2023-03-06 11:55:37 -0800

    q.2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in string
+ *
+ * @s: string
+ * @c: character
+ *
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+return (0);
+}
