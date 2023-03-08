2e4dadc8351137b7b3b9cb18ea228dc64ec11aca
Selam-afework <selam.a.gorfu@gmail.com>
2023-03-08 11:49:17 -0800

    q.5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,12 +10,29 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n <= 0)
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - square root function
+ * @n: input number
+ * @i: integer
+ * Return: sqrt
+ */
+
+int _sqrt(int n, int i)
+{
+	if (n < 0)
 	{
 		return (-1);
 	}
-	else
+	else if ((i * i) > n)
 	{
-		return (_sqrt_recursion(n));
+		return (-1);
 	}
+	else if ((i * i) == n)
+	{
+		return (i);
+	}
+	return (_sqrt(n, i + 1));
 }
