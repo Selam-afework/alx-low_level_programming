06c2b2c395c43d1db0fc0121721c7c5fa2941d5d
Selam-afework <selam.a.gorfu@gmail.com>
2023-03-10 12:35:35 -0800

    q.1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - to print its name
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char __attribute((__unused__)) *argv[])
+{
+        {
+                printf("%d\n", argc -1);
+        }
+        return (0);
+}
