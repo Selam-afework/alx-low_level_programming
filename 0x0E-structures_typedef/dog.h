901d9a5ef9b55606efe668931f45935d4bd6eb70
Selam-afework <selam.a.gorfu@gmail.com>
2023-03-21 12:47:37 -0700

    removing typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h>
-
 /**
  * struct dog - dog attributes
  *
@@ -19,12 +17,6 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - typedef for struct dog
- */
-
-typedef struct dog dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
