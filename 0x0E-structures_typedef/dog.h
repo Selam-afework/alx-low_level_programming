922b531b640f4467f05482fb5ea38c6de28e3de8
Selam-afework <selam.a.gorfu@gmail.com>
2023-03-21 12:18:04 -0700

    rearranging the list

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,27 @@
 #ifndef _HEADER_
 #define _HEADER_
 
+/** 
+ * struct dog - dog attributes
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: do owner
+ *
+ * description: dog attribution
+ */
+
 struct dog
 {
 	char *name;
-	char *owner;
 	float age;
+	char *owner;
 }
+
+/**
+ * dog_t - typedef for dog structures
+ */
+
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
