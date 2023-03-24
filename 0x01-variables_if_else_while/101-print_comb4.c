320db5dae0f165db042e2d14a86745af6099661a
Selam-afework <selam.a.gorfu@gmail.com>
2023-03-24 09:33:22 -0700

    adding summation line

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	int m, n, p;
+
 	for (m = 0; m <= 7; m++)
 	{
 		for (n = 0; n <= 8; n++)
@@ -19,6 +20,7 @@ int main(void)
 					putchar(m + '0');
 					putchar(n + '0');
 					putchar(p + '0');
+					if (m + n + p != 9 + 8 + 7)
 					{
 						putchar(',');
 						putchar(' ');
