9bc3b719f60447808ff52b95a46dda609288b464
Selam-afework <selam.a.gorfu@gmail.com>
2023-03-30 12:47:05 -0700

    number of elements in a linked list_t

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ *
+ * @h: pointer to a list
+ * Return: no of elements in a linked list_t
+ */
+
+size_t list_len(const list_t *h)
+{
+	const list_t *temp;
+
+	unsigned int counter = 0;
+
+	temp = h;
+	while (temp)
+	{
+		counter++;
+
+		temp = temp->next;
+	}
+	return (counter);
+}
