e3e6bb009cb302d9ae2dbea06947385ca1d9d292
Selam-afework <selam.a.gorfu@gmail.com>
2023-06-09 10:53:02 -0700

    adds no given

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ *
+ * @argc: no of arguments passed
+ * @argv: command line arguments
+ *
+ * Return: either 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 1)
+	{
+		printf("0\n");
+
+	}
+	else
+	{
+		int i, sum;
+
+		for (i = 0; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+}
